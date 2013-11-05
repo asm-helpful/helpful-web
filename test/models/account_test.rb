77fa783742eec7c460037ca5a8824956b4317fd3
Drew Blas <drew.blas@gmail.com>
2013-11-05 07:47:37 -0600

    Removed unique constraint on account name
    Per https://assemblymade.com/support-foo/wips/63 a slug is being added
    which is the identifier for the account.  It'll be URL-safe and unique
    across the app.
    
    Therefore I propose the "name" attribute is repurposed simply as the
    "display name" of the account.  It therefore should not be unique.

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -8,13 +8,4 @@ describe Account do
   it "must be valid" do
     @account.valid?.must_equal true
   end
-
-  it "must have a unique name" do
-    Account.create name: 'unique'
-    @account.name = 'unique'
-
-    assert_raises(ActiveRecord::RecordNotUnique) do
-      @account.save
-    end
-  end
 end
