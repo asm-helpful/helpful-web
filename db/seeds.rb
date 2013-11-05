26520d324c83fec2ae6a011ff7d247bceac6f406
Drew Blas <drew.blas@gmail.com>
2013-11-05 08:13:07 -0600

    Added a user to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # --- Account ---
 supportly = Account.create(name: 'Supportly')
 
+# --- User ---
+user = User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
+Membership.create!(account: supportly, user: user, role: 'owner')
+
 # --- Conversations ---
 messages = [
   [ ['Chris', "Hey I'm having trouble logging in."],
