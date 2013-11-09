1fadf1ba1b497c15ba3baea26ce30a1daf80b86c
Chris Lloyd <christopher.lloyd@gmail.com>
2013-11-08 17:49:02 -0800

    Stop initialization on precompile for all envs.
    It shouldn't be a surprise in dev that it doesn't work in production.

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module Supportly
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Stop assets from accessing models. Useful for Heroku and Devise.
+    config.assets.initialize_on_precompile = false
+    
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Supportly::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  # For Heroku & Devise
-  config.assets.initialize_on_precompile = false
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
