098c871f6642040359bfa7dd6a59baac50372fc4
Chris Lloyd <christopher.lloyd@gmail.com>
2013-11-10 21:59:05 -0800

    Fix failing test #65.

--- test/models/conversation_test.rb
+++ test/models/conversation_test.rb
@@ -22,20 +22,20 @@ describe Conversation do
   it "should only return unarchived conversations" do
     FactoryGirl.create :conversation
     FactoryGirl.create :archived_conversation
-    Conversation.unarchived.each do |c|
+    Conversation.open.each do |c|
       assert !c.archived?
     end
   end
 
   it "adds the correct conversation number on create based on account_id" do
     @account = FactoryGirl.create(:account)
-    
+
     @conversation_1 = FactoryGirl.create(:conversation, account: @account)
     assert_equal 1, @conversation_1.number
-    
+
     @conversation_2 = FactoryGirl.create(:conversation, account: @account)
     assert_equal 2, @conversation_2.number
-    
+
     @conversation_3 = FactoryGirl.create(:conversation)
     assert_equal 1, @conversation_3.number
   end
