fa202238ce8201252bba6bedca71c5caccba9715
Jared Smith <jaredsmithse@icloud.com>
2013-11-13 01:38:19 -0800

    Add logic to create a new person along with the account and user (and sync emails between user and person).

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,17 +2,25 @@ class AccountsController < ApplicationController
   def new
     @account = Account.new
     @new_account_user = User.new
+    @person = Person.new
   end
 
   def create
     user_params = params.require(:user).permit(:email, :password, :password_confirmation)
     account_params = params.require(:account).permit(:name)
+    person_params = params.require(:person).permit(:name)
 
-    @new_account_user    = User.new user_params
+    @new_account_user = User.new user_params
     @account = Account.new account_params
-    @account.new_account_user = @new_account_user
+    @person = Person.new person_params
 
-    if @new_account_user.valid? && @account.valid? && @account.save
+    @person.email = @new_account_user.email
+    
+    @account.new_account_user = @new_account_user
+    @new_account_user.person = @person
+
+
+    if @new_account_user.valid? && @account.valid? && @person.valid? && @account.save
       redirect_to root_url, notice: 'You have successfully signed up!  Try logging in!'
     else
       render 'new'
