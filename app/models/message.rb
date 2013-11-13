6b8e96763d190c0ba14adb061ad801730d23a1ac
Austin Lin <austin@austinylin.com>
2013-11-13 01:43:08 -0600

    Send a webhook for each new message

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,25 @@ class Message < ActiveRecord::Base
 
   belongs_to :conversation, touch: true
   belongs_to :person
+
+  after_create :send_webhook, unless: Proc.new { |m| m.conversation.account.web_hook_url.nil? }
+
+  def webhook_data
+    {
+      id: self.id,
+      conversation_id: self.conversation_id,
+      content: self.content,
+      from: self.from,
+      created_at: self.created_at.utc.to_i,
+      updated_at: self.updated_at.utc.to_i
+    }
+  end
+
+  def send_webhook
+    WebhookWorker.perform_async(
+      self.conversation.account.id,
+      "message.new",
+      self.webhook_data
+    )
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 describe Message do
   before do
-    @message = Message.new
+    @message = FactoryGirl.build(:message)
   end
 
   it "is valid" do
     assert @message.valid?
+  end
+
+  it "returns a hash of webhook_data" do
+    @message.save
+    webhook_data = @message.webhook_data
+    webhook_data.each do |k,v|
+      assert v.class.in?([Hash, Array, String, Integer, NilClass, Fixnum]),
+        "Value is an invalid type: #{k} - #{v.class}"
+    end
   end
 end
