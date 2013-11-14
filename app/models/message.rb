344fa6a9720ff935889348b2b1ebd2929b914aae
Matthew Deiters <mdeiters@gmail.com>
2013-11-13 20:09:50 -0800

    made it easier to setup development environment with search

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,11 +7,10 @@ class Message < ActiveRecord::Base
   belongs_to :conversation, touch: true
   belongs_to :person
   
-  private
-    def update_search_index
-      if ENV['ELASTICSEARCH_URL']
-        es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
-        es.index( { index: 'helpful', type:  'message', id: self.id, body: { content: self.content} } )
-      end
+  def update_search_index
+    if ENV['ELASTICSEARCH_URL']
+      es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
+      es.index( { index: 'helpful', type:  'message', id: self.id, body: { content: self.content} } )
     end
+  end
 end
