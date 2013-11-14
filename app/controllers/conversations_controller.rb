c84c4a85d1be1accbe84df7e7d4d8e80164de670
Brandon Fish <brandon.j.fish@gmail.com>
2013-11-13 19:26:25 -0600

    Adding initial search of messages for #71

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,15 @@ class ConversationsController < ApplicationController
   before_action :load_account
 
   def index
-    @conversations = @account.conversations.open.includes(:messages)
+    if params['q'] and not params['q'].empty?
+      @query = params['q']
+      es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
+      response = es.search body: { query: { match: { content: @query.to_s } } }
+      ids = response['hits']['hits'].map { |x| x["_id"] }
+      @conversations = @account.conversations.joins(:messages).where(messages: {:id => ids})
+    else
+      @conversations = @account.conversations.open.includes(:messages)
+    end
   end
 
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,9 @@
 require 'activerecord/uuid'
+require 'elasticsearch'
 
 class Message < ActiveRecord::Base
   include ActiveRecord::UUID
-
+  after_save :update_search_index
   belongs_to :conversation, touch: true
   belongs_to :person
 
@@ -26,4 +27,12 @@ class Message < ActiveRecord::Base
       self.webhook_data
     )
   end
+  
+  private
+    def update_search_index
+      if ENV['ELASTICSEARCH_URL']
+        es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
+        es.index( { index: 'helpful', type:  'message', id: self.id, body: { content: self.content} } )
+      end
+    end
 end
