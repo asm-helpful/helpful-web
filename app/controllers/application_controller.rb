504e03ac4a067456e22de7b1cbfc66cbd0e6bd15
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-11-14 10:20:31 -0200

    Redirects user to conversation index after sign in and sign up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Devise: Where to redirect users once they have logged in
+  def after_sign_in_path_for(resource)
+    conversations_index_path
+  end
 end
