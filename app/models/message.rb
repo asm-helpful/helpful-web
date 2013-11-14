f122410d130825a133a930e2d6189c256b517a0c
Brandon Fish <brandon.j.fish@gmail.com>
2013-11-13 19:26:25 -0600

    Adding initial search of messages for #71

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,17 @@
 require 'activerecord/uuid'
+require 'elasticsearch'
 
 class Message < ActiveRecord::Base
   include ActiveRecord::UUID
-
+  after_save :update_search_index
   belongs_to :conversation, touch: true
   belongs_to :person
+  
+  private
+    def update_search_index
+      if ENV['ELASTICSEARCH_URL']
+        es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
+        es.index( { index: 'helpful', type:  'message', id: self.id, body: { content: self.content} } )
+      end
+    end
 end
