bde341c5bbed072a315eb5ea78ad3a79547c923c
Dave Newman <dave@snappyco.de>
2013-11-16 13:41:39 -0800

    refactoring conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,24 +2,35 @@ class ConversationsController < ApplicationController
   before_action :load_account
 
   def index
-    if params['q'] and not params['q'].empty?
-      @query = params['q']
-      es = Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
-      response = es.search body: { query: { match: { content: @query.to_s } } }
-      ids = response['hits']['hits'].map { |x| x["_id"] }
-      @conversations = @account.conversations.joins(:messages).where(messages: {:id => ids})
-      if current_user
-        Analytics.track(user_id: current_user.id, event: 'Searched For', properties: { query: @query.to_s })
-      end
-    else
+    if params['q'].blank?
       @conversations = @account.conversations.open.includes(:messages)
-      if current_user
-        Analytics.track(user_id: current_user.id, event: 'Read Conversations Index')
+
+      # TODO move analytics to javascript?
+      # TODO standardise analytics event names
+      Analytics.track(user_id: current_user.id, event: 'Read Conversations Index') if signed_in?
+
+    else
+      @query = params['q'].to_s.strip
+
+      message_ids = query_messages(@query)
+      @conversations = @account.conversations.joins(:messages).where(messages: { id: message_ids })
+
+      if signed_in?
+        Analytics.track(user_id: current_user.id, event: 'Searched For', properties: { query: @query.to_s })
       end
     end
   end
 
   private
+
+  def query_messages(query)
+    response = elasticsearch.search body: { query: { match: { content: query } } }
+    response['hits']['hits'].map { |x| x["_id"] }
+  end
+
+  def elasticsearch
+    @es ||= Elasticsearch::Client.new hosts: [ ENV['ELASTICSEARCH_URL'] ]
+  end
 
   def load_account
     @account = Account.first! # simpfy until for now
