11acb37339460ef627c182eede98c93a0282f2c0
Dave Newman <dave@snappyco.de>
2013-11-19 10:50:00 -0500

    unskip conversation controller 404 test

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -18,7 +18,6 @@ describe ConversationsController do
   end
 
   test "should raise RecordNotFound with no supplied account" do
-    skip "Disabled until multi-account support setup"
     assert_raises(ActiveRecord::RecordNotFound) do
       get :index
     end
