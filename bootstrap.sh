6c2ae5261e611e13b91faa4e5ebcd198142c97d6
Chris Lloyd <christopher.lloyd@gmail.com>
2013-11-19 15:07:42 -0500

    Vagrant WIP.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install build-essential
+log () {
+  echo "-----> $1"
+}
 
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-make
-sudo make install
-cd utils
-yes '' | sudo ./install_server.sh
+log "Installing Postgresql"
+sudo apt-get install postgresql-contrib
 
+log "Installing Redis"
+sudo apt-get install redis-server
+
+log "Starting Redis"
+sudo service redis-server start
+
+log "Installing ElasticSearch"
 sudo apt-get install curl python-software-properties -y
 sudo apt-get install openjdk-6-jre-headless -y
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
 sudo dpkg -i elasticsearch-0.90.7.deb
+
+log "Starting ElasticSearch"
 sudo service elasticsearch start
+
+
+cd /vagrant
+
+log "Installing gems"
+bundle install
+
+log "Configuring database"
+cp config/database.yml.example config/database.yml
+createuser --createdb --no-password root --user vagrant
+bin/rake db:setup
+
+log "Configuring environment"
+cp .env.example .env
+
+log "Starting Helpful"
+sudo foreman export upstart /etc/init \
+  --app helpful \
+  --user vagrant \
+  --log /var/helpful/log
