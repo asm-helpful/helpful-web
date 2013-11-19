42442f867009b0b9b5960da27b336a6468743505
Chris Lloyd <christopher.lloyd@gmail.com>
2013-11-19 17:06:50 -0500

    Remove seed data.
    Seed data should be used in production as well as development so this
    data really is development data. At the moment it’s another thing to
    maintain and another thing that could go wrong stopping people from
    contributing to the proejct.

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-# --- Account ---
-supportly = Account.create(name: 'Supportly')
-
-# --- User ---
-user = User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
-Membership.create!(account: supportly, user: user, role: 'owner')
