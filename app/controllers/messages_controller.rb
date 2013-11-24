43aa94d454202b39819fb9e8f6697b96185a8d25
Chris Lloyd <christopher.lloyd@gmail.com>
2013-11-23 18:56:31 -0800

    Fix tests by removing dead code.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,6 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
-  def index
-    @messages = Message.all
-
-    if current_user
-      Analytics.track(user_id: current_user.id, event: 'Listed Messages')
-    end
-  end
-
   def create
     @message = Message.new message_params
     conversation = Conversation.find(message_params['conversation_id'])
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 describe MessagesController do
-  test "index is ok" do
-    get :index
-    assert_equal response.status, 200
-  end
 end
