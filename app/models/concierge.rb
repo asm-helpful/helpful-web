4f1a60cfcc8e9c3c31a9f385c72ea402fa6c8354
Austin Lin <austin@austinylin.com>
2013-12-06 21:31:31 -0600

    Update Concierge to support Conversation#mailbox addresses

--- app/models/concierge.rb
+++ app/models/concierge.rb
@@ -1,26 +1,27 @@
 # Figures out which conversation an incoming message belongs to. If it can't
 # match it to a conversation it creates a new one.
 class Concierge
-  EMAIL_REGEX = Regexp.new(/\S+(\+\S+)?\+(?<number>\d)/).freeze
   def initialize(account, params)
     @account = account
     @conversations = account.conversations
     @params = params
   end
 
-  def find_conversation
-    number = @params[:conversation] || number_from_email(@params[:recipient])
-    @conversations.find_or_create_by(number: number)
-  end
-
-  private
-
-  # Private: Extract the conversation number from an email adddress.
+  # Public: Finds or creates a conversation based on the params hash. The order
+  # of precedence is:
+  #   - An explicit conversation number (params[:conversation]).
+  #   - A Conversation#mailbox email address (params[:recipient]).
+  # If no conversation has been found, create a new one.
   #
-  # Returns conversation number or nil.
-  def number_from_email(input_address)
-    email = Mail::Address.new input_address
-    EMAIL_REGEX.match(email.address).try { |match| match[:number] }
-  end
+  # Returns a Conversation.
+  def find_conversation
+    # If we have an explicit conversation number try that first
+    conversation = @conversations.where(number: @params.fetch(:conversation, nil)).first
 
+    # If that didn't work, match on the recipient email address, looking for replies
+    conversation ||= Conversation.match_mailbox(@params.fetch(:recipient, nil))
+
+    # If that didn't work, create a new conversation
+    conversation ||= @conversations.create
+  end
 end
