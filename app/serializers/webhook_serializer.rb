2e1c09f2291bbda959b6ec25b9a5ee8a79fc941f
Drew Blas <drew.blas@gmail.com>
2013-12-14 11:05:26 -0600

    Added HalSerializer that adds link helpers

--- app/serializers/webhook_serializer.rb
+++ app/serializers/webhook_serializer.rb
@@ -1,8 +1,12 @@
-class WebhookSerializer < ActiveModel::Serializer
+class WebhookSerializer < ActiveModel::HalSerializer
   attributes :id
   attributes :account_id, :event, :body, :signature
   attributes :response
   include TimestampedSerializer
+
+  link :self do |webhook|
+    { href: "/webhooks/#{webhook.id}" }
+  end
 
   def response
     {
