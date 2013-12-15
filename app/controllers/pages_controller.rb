4f9ff5c2462594dccd07dcfa1d8f374045af5f3f
Chris Lloyd <christopher.lloyd@gmail.com>
2013-12-15 15:26:06 -0800

    Move doc rendering to the Apiary file in the root.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 
   def home
-    @beta_invite = BetaInvite.new
   end
 
   def embed
@@ -9,9 +8,9 @@ class PagesController < ApplicationController
   end
 
   def docs
-    renderer = Redcarpet::Render::HTML.new#, :no_links => true, :hard_wrap => true
-    markdown = Redcarpet::Markdown.new renderer#, :autolink => true, :space_after_headers => true
-    text = markdown.render(File.read(Rails.root.join('app','views','pages','docs.md'))).html_safe
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    text = markdown.render(Rails.root.join('apiary.apib').read).html_safe
     render inline: text, layout: true
   end
 
