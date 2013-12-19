fb432b1bd5cf3ad56af14a567d7307d6e5447fc6
Austin Lin <austin@austinylin.com>
2013-12-18 21:14:44 -0600

    Improve Vagrant bootstrap script

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,48 +1,70 @@
 #!/usr/bin/env bash
 
-# Update apt-get and install the basics
+log () {
+  echo -e "\n\e[35m-----> $1\n"
+}
+
+log "Updating Aptitude and installing the basics"
 apt-get update
 apt-get install curl python-software-properties -y
 
-# Install Redis
+log "Installing Redis"
 /usr/bin/add-apt-repository ppa:chris-lea/redis-server
 apt-get update
 apt-get install redis-server -y
+
+log "Starting Redis"
 service redis-server start
 
-# Install ElasticSearch
+log "Installing ElasticSearch"
 if dpkg -s elasticsearch > /dev/null 2>&1; then
-  echo "Looks like elasticsearch is already installed. Trying to start it."
+  log "ElasticSearch is already installed. Trying to start it."
   service elasticsearch start
 else
   apt-get install openjdk-6-jre-headless -y
   wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
   dpkg -i elasticsearch-0.90.7.deb
+  log "Starting ElasticSearch"
   service elasticsearch start
 fi
 
-# Setup Helpful
+
+log "Setting up Helpful"
+
+# Get in the right directory
 cd /vagrant
+
+# Bundle
 su vagrant -lc "cd /vagrant && bundle install"
 
-cp config/database.yml.example config/database.yml &&
-su vagrant -lc "cd /vagrant && rake db:setup"
-
+# Setup .env
 cp .env.example .env
-su vagrant -lc "cd /vagrant && rake search:reindex"
+
+log "Setting up the Database"
+cp config/database.yml.example config/database.yml &&
+su vagrant -lc "cd /vagrant && bundle exec rake db:setup"
+
+# Trigger a reindex for ElasticSearch
+su vagrant -lc "cd /vagrant && bundle exec rake search:reindex"
+
+log "Starting Helpful"
+su vagrant -lc "cd /vagrant && sudo foreman export upstart /etc/init \
+  --app helpful \
+  --user vagrant \
+  --log /vagrant/log"
+
+start helpful
+
 cat <<EOF
 -------------------------------------------------------------------------------
                                 Helpful
                           An Assembly Product
 
-Let's get started, just run the following commands to get up and running:
+Thanks for installing Helpful.
 
-$ vagrant ssh
-$ cd /vagrant
-$ foreman start
+Open up http://localhost:5000 in your browser to get started.
 
-If everything looks ok, open up http://localhost:5000 in your browser.
 
-Welcome to Helpful.
+Problems? Please contact us at helpful@helpful.io.
 -------------------------------------------------------------------------------
 EOF
