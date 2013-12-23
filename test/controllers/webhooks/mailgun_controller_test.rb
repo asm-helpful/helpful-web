5d65822ad83eb4deddcded56a947083ec47eca36
Austin Lin <austin@austinylin.com>
2013-12-23 02:36:34 -0600

    Use Mailgun stripped-text for message content

--- test/controllers/webhooks/mailgun_controller_test.rb
+++ test/controllers/webhooks/mailgun_controller_test.rb
@@ -2,43 +2,50 @@ require 'test_helper'
 require 'openssl'
 
 describe Webhooks::MailgunController do
-  def post_webhook(args = {})
 
-    # The mailgun API only uses timestamp and token as the inputs to HMAC
-    # as a result we can just generate a static signature to test with.
-    #
-    # To generate a signature:
-    #
-    #  signature = OpenSSL::HMAC.hexdigest(
-    #                        OpenSSL::Digest::Digest.new('sha256'),
-    #                        api_key,
-    #                        '%s%s' % [timestamp, token])
+  def post_webhook(args = {})
+    post :create, default_body.merge(args)
+  end
+
+
+  # Construct the default request body including a signature
+  def default_body
+    account = FactoryGirl.create(:account)
+
+    {
+      from:             "test@test.com",
+      subject:          "test email",
+      recipient:        "#{account.slug}@helpful.io",
+
+      'body-plain'      =>  "Test email.\n Thanks",
+      'stripped-text'   =>  "Test email.\n Thanks",
+      'message-headers' =>  [
+        ["From", "test@test.com"],
+        ["Recieved", "by luna.mailgun.net"],
+        ["Recieved", "from mail.google.com by mxa.mailgun.org"]
+      ],
+    }.merge(generate_signature)
+  end
+
+  def generate_signature
+    # The mailgun API only uses timestamp and token as the inputs to HMAC.
+
     api_key   = "key-bb8ef21a0c161b2f9b0950fee7a52ab1"
-    signature = "e34ce2cbae65ced873f2dd3657a4d145fe0245ff3d896b6a27da82b246892df0"
-    timestamp = 1385618722
+    timestamp = Time.now.utc.to_i
     token     = "3fb7496d2e8761c4ca99e2b8265df4df61ba886dc27224a9b2"
+
+    signature = OpenSSL::HMAC.hexdigest(
+      OpenSSL::Digest::Digest.new('sha256'),
+      api_key,
+      '%s%s' % [timestamp, token])
 
     ENV['MAILGUN_API_KEY'] = api_key
 
-    @account = FactoryGirl.create(:account)
-    body = {
-      from:       args[:from]       || "test@test.com",
-      subject:    args[:subject]    ||  "test email",
-      recipient:  args[:recipient]  || "#{@account.slug}@helpful.io",
-
-      'body-plain'      =>  args['body-plain'] || "Test email.\n Thanks",
-      'message-headers' =>  args['message-headers'] || [
-                              ["From", "test@test.com"],
-                              ["Recieved", "by luna.mailgun.net"],
-                              ["Recieved", "from mail.google.com by mxa.mailgun.org"]
-                            ],
-
-      timestamp:  args[:timestamp]  || timestamp,
-      token:      args[:token]      || token,
-      signature:  args[:signature]  || signature
+    {
+      timestamp:        timestamp,
+      token:            token,
+      signature:        signature
     }
-
-    post :create, body
   end
 
   describe "#verify_hook" do
@@ -63,7 +70,7 @@ describe Webhooks::MailgunController do
 
       it "persists the correct content" do
         content = "This is a test email.\n Thanks."
-        post_webhook('body-plain' => content)
+        post_webhook('stripped-text' => content, 'body-plain' => 'Other Text')
         assert_equal content, Messages::Email.last.content
       end
 
@@ -82,8 +89,9 @@ describe Webhooks::MailgunController do
       end
 
       it "associates the message with the correct account" do
-        post_webhook
-        assert_equal @account, Messages::Email.last.account
+        account = create(:account)
+        post_webhook(recipient: "#{account.slug}@helpful.io")
+        assert_equal account, Messages::Email.last.account
       end
 
       it "associates the message with the correct conversation" do
