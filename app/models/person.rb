c5beae63c6f7da41d2b8991e31a62746173eebc1
vanstee <patrick@vanstee.me>
2013-12-27 09:57:35 -0800

    Add role helper methods for memberships and people

--- app/models/person.rb
+++ app/models/person.rb
@@ -20,6 +20,16 @@ class Person < ActiveRecord::Base
 
   before_save :parse_email
 
+  Membership::ROLES.each do |role_name|
+    define_method("account_#{role_name}?") do |account|
+      account_member_role?(account, role_name)
+    end
+  end
+
+  def account_member_role?(account, role)
+    memberships.where(account_id: account.id, role: role).exists?
+  end
+
   private
 
   # Private: Make sure we only save the address portion of an email address.
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -8,4 +8,17 @@ describe Membership do
   it "must be valid" do
     @membership.valid?.must_equal true
   end
+
+  describe "role helpers" do
+    it "returns true if the membership has the matching role" do
+      @membership.role = 'owner'
+      assert @membership.owner?
+
+      @membership.role = 'agent'
+      assert @membership.agent?
+
+      @membership.role = 'customer'
+      assert @membership.customer?
+    end
+  end
 end
