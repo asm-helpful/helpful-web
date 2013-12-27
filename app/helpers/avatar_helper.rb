5e089b282283692f7709b793ec8d57ba49855fcc
Chris Lloyd <christopher.lloyd@gmail.com>
2013-12-27 18:32:08 -0500

    Default avatars
    Creates the concept of the “default sized” avatar. Includes new helpers
    and a more flexible base avatar helper.

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,21 +1,28 @@
 module AvatarHelper
 
+  def avatar_default(person)
+    avatar(person, 45, 'avatar-default')
+  end
+
+
   # Public: View helper to provide an avatar div & img for a given user.
   #
-  # person - a Person or User object. Must have an email attribute.
+  # person       - a Person or User object. Must have an email method.
   #
-  # size   - the non-retina size of the avatar
+  # size         - the non-retina size of the avatar
+  #
+  # html_classes - optional extra html classes. Typically `avatar-` variations.
   #
   # Returns a img tag wraped in a div with the class of avatar.
-  def avatar(person, size)
-    content_tag(:div, class: 'avatar') do
-      image_tag gravatar_url(person.email, size)
+  def avatar(person, size, *html_classes)
+    content_tag(:div, class: ['avatar', *html_classes]) do
+      image_tag gravatar_url(person.email, size), width: size, height: size
     end
   end
 
   # Internal: Helper to provide the gravatar_url for a given person.
   #
-  # person - a Person or User object. Must have an email attribute.
+  # person - a Person or User object. Must have an email method.
   #
   # Returns a url to the PNG of the user's gravatar.
   def gravatar_url(email, size)
@@ -23,6 +30,11 @@ module AvatarHelper
     "https://secure.gravatar.com/avatar/#{id}.png?s=#{size}"
   end
 
+  # Internal: Helper that generates a gravatar id for an email
+  #
+  # email - a string
+  #
+  # Returns a string id.
   def gravatar_id(email)
     Digest::MD5::hexdigest(email).downcase
   end
--- test/helpers/avatar_helper_test.rb
+++ test/helpers/avatar_helper_test.rb
@@ -1,19 +1,33 @@
 require 'test_helper'
 
-class AvatarHelperTest < ActionView::TestCase
+describe AvatarHelper do
 
   before do
     @person = OpenStruct.new(email: 'test@example.com')
   end
 
-  test "generates a gavatar url for a user" do
+  test "#gravatar_url" do
     assert_equal 'https://secure.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0.png?s=60', gravatar_url(@person.email, 60)
   end
 
-  test "generates markup for an avatar" do
-    output = avatar(@person, 60)
-    assert_match %r{class="avatar"}, output
-    assert_match %r{img}, output
+  test "#avatar returns an element with the avatar class" do
+    html = avatar(@person, 60)
+    assert_match %r{avatar}, html
+  end
+
+  test "#avatar returns an img element" do
+    html = avatar(@person, 60)
+    assert_match %r{img}, html
+  end
+
+  test "#avatar returns an img element with the size" do
+    html = avatar(@person, 60)
+    assert_match %r{width="60"}, html
+  end
+
+  test "#avatar_default returns an element with the avatar-default class" do
+    html = avatar_default(@person)
+    assert_match /avatar-default/, html
   end
 
 end
