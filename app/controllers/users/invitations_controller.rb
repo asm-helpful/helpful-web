02377fd7a07a53c085829b9e358325b67e8f6349
Chris Lloyd <christopher.lloyd@gmail.com>
2014-01-07 12:14:51 -0800

    Create a new Person object for new members. #199

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -15,4 +15,25 @@ class Users::InvitationsController < Devise::InvitationsController
     end
   end
 
+  def update
+    self.resource = resource_class.accept_invitation!(update_resource_params)
+
+    account = resource.primary_owned_account
+    person = Person.create(
+      account: account,
+      user:    resource,
+      name:    params.fetch(:name),
+      email:   resource.email
+    )
+
+    if resource.errors.empty?
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message :notice, flash_message
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_accept_path_for(resource)
+    else
+      respond_with_navigational(resource){ render :edit }
+    end
+  end
+
 end
