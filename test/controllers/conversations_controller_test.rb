c320b71d7503746baad5514e348780b6f5163949
vanstee <patrick@vanstee.me>
2014-01-15 22:19:36 -0500

    Assign the first agent to view a conversation

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 describe ConversationsController, :index do
   setup do
     @current_user = create(:user)
+    create(:person, user: @current_user)
     sign_in(@current_user)
     @conversation = create(:conversation_with_messages)
   end
@@ -16,5 +17,14 @@ describe ConversationsController, :index do
     get :index, account: @conversation.account.slug
     assert_response :success
     assert_not_nil assigns(:conversations)
+  end
+
+  test "assigns an agent to an unread conversation" do
+    get :show,
+      id: @conversation.number,
+      account: @conversation.account.slug
+
+    assert_response :success
+    assert_equal @conversation.reload.agent, @current_user
   end
 end
