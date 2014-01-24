9dbe6a55b8dd08a7148a436a68d5294366a1622b
Manish Valechha <manishval@hotmail.com>
2014-01-24 00:00:59 -0400

    Initial Backbone.js setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,15 @@
 //= require jquery
 //= require jquery_ujs
+//
+//= require underscore
+//= require backbone
+//= require helpful
+//
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree ../templates
 
 $(document).ready(function() {
 
