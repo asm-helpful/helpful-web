3e2f942df8c006386964a1518f7e225707775e1e
Nícolas Iensen <nicolas@engage.is>
2014-02-19 14:17:26 -0300

    Resize list of tickets and column wrapper to fit the window even when it's resized, also add a Y scrool to the list of tickets #232

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
   // Column Wrappers should be the height of the window.
   // TODO Handle window resizes
   // FIXME Investigate using the CSS3 flow properties so we don't need to us JS.
-  $('.column-wrapper').height(window.innerHeight);
+  onWindowResize();
+  $(window).resize(onWindowResize);
+
+  function onWindowResize(){
+    $('.column-wrapper').height(window.innerHeight);
+    $('.column-subnav .list').height(window.innerHeight - ($('.column-subnav').height() - $('.column-subnav .list').height()));
+  }
 
 });
