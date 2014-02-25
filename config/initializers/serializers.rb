9b7c2a20b6545d242aaef54916fb700e27351fc9
Chris Lloyd <christopher.lloyd@gmail.com>
2014-02-24 18:46:46 -0800

    Include root in Serializers.
    Thanks @vanstee for pointing out the security venerability in exposing
    root level arrays in JSON.

--- config/initializers/serializers.rb
+++
@@ -1,7 +0,0 @@
-ActiveSupport.on_load(:active_model_serializers) do
-  # Disable for all serializers (except ArraySerializer)
-  ActiveModel::Serializer.root = false
-
-  # Disable for ArraySerializer
-  ActiveModel::ArraySerializer.root = false
-end
