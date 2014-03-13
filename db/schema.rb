8fe0327a40bcd8f6afac56354ebcb6c04f83aaab
Chris Lloyd <christopher.lloyd@gmail.com>
2014-03-13 13:00:30 -0700

    And actually commit schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310154020) do
+ActiveRecord::Schema.define(version: 20140313195537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -233,6 +233,7 @@ ActiveRecord::Schema.define(version: 20140310154020) do
     t.integer  "invitation_limit"
     t.uuid     "invited_by_id"
     t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
