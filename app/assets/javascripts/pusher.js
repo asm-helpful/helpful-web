9bc69a2cdf0c85c39b321021b81b8f425e982a97
Nícolas Iensen <nicolas@engage.is>
2014-03-14 09:12:41 -0300

    Add support to the empty list state in Javascript #143

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -1,19 +1,30 @@
 $(function(){
+  // Pusher global vars
+  var pusher = new Pusher($(".list").data("pusher-key"));
+  var listUrl = $(".list").data("url");
+  var channel = pusher.subscribe($(".list").data("pusher-channel"));
+
+  // Pusher logger
   Pusher.log = function(message) {
     if (window.console && window.console.log) {
       window.console.log(message);
     }
   };
 
+  // Pusher event bind
   if($(".list").length){
-    var pusher = new Pusher($(".list").data("pusher-key"));
-    var listUrl = $(".list").data("url");
-    var channel = pusher.subscribe($(".list").data("pusher-channel"));
+    channel.bind('new_message', function(data) { loadList(); });
+    loadList();
+  }
 
-    channel.bind('new_message', function(data) {
-      $(".list").load(listUrl);
+  // Ajax load list function
+  function loadList(){
+    $(".list").load(listUrl, function(){
+      if($(".list .list-item").length == 0){
+        $(".panel.empty-state").show();
+      } else {
+        $(".panel.empty-state").hide();
+      }
     });
-
-    $(".list").load(listUrl);
   }
 });
