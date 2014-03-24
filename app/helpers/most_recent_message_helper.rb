957c7df63a6082c974cf3bbd94585fb48d82effb
vanstee <patrick@vanstee.me>
2014-03-24 16:27:38 -0400

    Move last activity timestamp to reusable method

--- app/helpers/most_recent_message_helper.rb
+++ app/helpers/most_recent_message_helper.rb
@@ -1,10 +1,6 @@
 module MostRecentMessageHelper
   def most_recent_message_class(conversation)
-    most_recent_message = conversation.most_recent_message
-    most_recent_message_at = most_recent_message && most_recent_message.updated_at
-    last_activity_at = most_recent_message_at || conversation.updated_at
-
-    case Time.now - last_activity_at
+    case Time.now - conversation.last_activity_at
     when 0...3.hours      then 'recent'
     when 3.hours...3.days then 'normal'
     else                       'stale'
--- spec/helpers/most_recent_message_helper_spec.rb
+++ spec/helpers/most_recent_message_helper_spec.rb
@@ -3,16 +3,9 @@ require 'spec_helper'
 describe MostRecentMessageHelper do
   it "returns a css class that describes the age of the message" do
     { 'recent' => 5.minutes.ago, 'normal' => 1.day.ago, 'stale' => 4.days.ago }.each do |description, time|
-      message = double("message", updated_at: time)
-      conversation = double("conversation", most_recent_message: message)
+      conversation = double("conversation", last_activity_at: time)
 
       expect(most_recent_message_class(conversation)).to eq(description)
     end
-  end
-
-  it "defaults to using the last updated timestamp of the conversation if a message is not found" do
-    conversation = double("conversation", most_recent_message: nil, updated_at: 5.days.ago)
-
-    expect(most_recent_message_class(conversation)).to eq('stale')
   end
 end
