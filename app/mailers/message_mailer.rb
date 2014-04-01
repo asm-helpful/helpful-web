148ad5dafa45e150d8b4b0efcf11538385c97700
Jess Brown <jess@brownwebdesign.com>
2014-04-01 14:01:24 -0400

    format outgoing messages from the app in html wip #352

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,7 @@
 class MessageMailer < ActionMailer::Base
   include SummaryHelper
   include NicknameHelper
+  include MarkdownHelper
 
   # Public: Triggered on Message create
   #
@@ -8,6 +9,7 @@ class MessageMailer < ActionMailer::Base
   def created(message_id, recipient_id)
     @message = Message.find(message_id)
     @recipient = Person.find(recipient_id)
+    @message_body = markdown(@message.content)
 
     @conversation = @message.conversation
     @account = @conversation.account
