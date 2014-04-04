0d2793069df2268b035b09238fd7b3db60cf10e2
vanstee <patrick@vanstee.me>
2014-04-04 17:24:36 -0400

    Render markdown where the helper is available

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -10,6 +10,7 @@ class MessageMailer < ActionMailer::Base
   def created(message_id, recipient_id)
     @message = Message.find(message_id)
     @recipient = Person.find(recipient_id)
+    @message_markdown = markdown(@message.content)
 
     @conversation = @message.conversation
     @account = @conversation.account
