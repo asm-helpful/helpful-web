f5b629418855978e77bbcd955de9b59f36d1ec64
vanstee <patrick@vanstee.me>
2014-04-07 12:08:09 -0400

    Clean up the avatar content tag calls

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -20,16 +20,15 @@ module AvatarHelper
   end
 
   def avatar_initials(person, size)
-    content_tag(:div, class: 'avatar-initials', style: "width: #{size}px; height: #{size}px; line-height: #{size}px") do
-      person.initials
-    end
+    content_tag(:div, person.initials, class: 'avatar-initials', style: avatar_style_constraints(size))
   end
 
   def avatar_image(person, size)
-    image_tag gravatar_url(person.email, size),
-      width: size,
-      height: size,
-      onerror: "this.style.display = 'none'; this.previousSibling.style.display = 'block';"
+    image_tag(gravatar_url(person.email, size), width: size, height: size, onerror: "this.style.display = 'none'; this.previousSibling.style.display = 'block';" )
+  end
+
+  def avatar_style_constraints(size)
+    ['width', 'height', 'line-height'].map { |property| "#{property}: #{size}px" }.join('; ')
   end
 
   # Internal: Helper to provide the gravatar_url for a given person.
