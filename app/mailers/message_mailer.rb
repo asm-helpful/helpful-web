af334a43011f4dc81dd6c6c652056cd76d4e93e1
Wesley Lancel <wesleylancel@gmail.com>
2014-04-14 19:27:11 +0200

    Set empty string when signature is not defined

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -18,8 +18,8 @@ class MessageMailer < ActionMailer::Base
 
     subject = summary(@conversation)
 
-    @signature_markdown = markdown(@account.signature)
-    @signature_text = stripdown(@account.signature)
+    @signature_markdown = @account.signature ? markdown(@account.signature) : ''
+    @signature_text = @account.signature ? stripdown(@account.signature) : ''
 
     to = Mail::Address.new(@recipient.email)
     to.display_name = @recipient.name
