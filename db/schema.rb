b5a56c38476d584b4db6cc7004bdad2b1a8ccb99
vanstee <patrick@vanstee.me>
2014-04-18 17:16:32 -0400

    Remove accidently added changes without migration

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20140417221953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+  enable_extension "pg_stat_statements"
   enable_extension "uuid-ossp"
 
   create_table "accounts", id: false, force: true do |t|
@@ -94,7 +95,6 @@ ActiveRecord::Schema.define(version: 20140417221953) do
     t.boolean  "hidden",     default: false, null: false
   end
 
-  add_index "conversations", ["account_id"], name: "index_conversations_on_account_id", using: :btree
   add_index "conversations", ["hidden"], name: "index_conversations_on_hidden", using: :btree
   add_index "conversations", ["user_id"], name: "index_conversations_on_user_id", using: :btree
 
@@ -119,8 +119,6 @@ ActiveRecord::Schema.define(version: 20140417221953) do
     t.datetime "updated_at"
     t.uuid     "person_id",       null: false
   end
-
-  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
 
   create_table "oauth_access_grants", force: true do |t|
     t.integer  "resource_owner_id",              null: false
