18b4419aab01881391bf01adbfdeae2fe55b6bb1
vanstee <patrick@vanstee.me>
2014-05-22 00:40:27 -0400

    Fix up textcomplete test

--- spec/models/textcomplete_search_spec.rb
+++ spec/models/textcomplete_search_spec.rb
@@ -3,24 +3,17 @@ require 'spec_helper'
 describe TextcompleteSearch do
   let(:account) { double('Account') }
 
-  describe '#query_type' do
-    it 'figures out the query type' do
-      search = TextcompleteSearch.new(account, '#billing')
-      expect(search.query_type).to eq('tag')
-    end
-  end
-
   describe '#results' do
     let!(:account) { create(:account) }
     let!(:canned_response) { create(:canned_response, account: account, key: 'refund') }
 
     it 'grabs matching objects for the query type' do
-      search = TextcompleteSearch.new(account, ':ref')
+      search = TextcompleteSearch.new(account, 'ref', 'canned_responses')
       expect(search.results.first.fetch(:value)).to eq('refund')
     end
 
     it 'returns an empty array if the query is empty' do
-      search = TextcompleteSearch.new(account, '')
+      search = TextcompleteSearch.new(account, '', 'tags')
       expect(search.results).to eq([])
     end
   end
