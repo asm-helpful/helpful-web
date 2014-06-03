674975a3dee2d4c26b84270afb64943908a3421f
vanstee <patrick@vanstee.me>
2014-06-03 10:51:47 -0400

    Use new alert styling for all flash messages

--- app/helpers/conversation_helper.rb
+++ app/helpers/conversation_helper.rb
@@ -7,4 +7,13 @@ module ConversationHelper
       when :alert then "alert alert-error"
     end
   end
+
+  def flash_icon_class(level)
+    case level.to_sym
+      when :notice then "ss-info"
+      when :success then "ss-check"
+      when :error then "ss-alert"
+      when :alert then "ss-alert"
+    end
+  end
 end
