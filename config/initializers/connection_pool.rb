005eb7294e41b55419b4ce9313952cec22f0b7f7
vanstee <patrick@vanstee.me>
2014-06-10 17:47:34 -0400

    Reconnect after setting connection pool options

--- config/initializers/connection_pool.rb
+++
@@ -1,5 +0,0 @@
-config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-config['pool']              = ENV['DB_POOL'] || 5
-ActiveRecord::Base.establish_connection(config)
