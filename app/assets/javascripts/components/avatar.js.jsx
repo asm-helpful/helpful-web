67df9fcd5fb91aa50570314fcb12ebe55526d523
vanstee <patrick@vanstee.me>
2014-06-18 22:17:11 -0400

    Add small size for displaying a message author

--- app/assets/javascripts/components/avatar.js.jsx
+++ app/assets/javascripts/components/avatar.js.jsx
@@ -2,8 +2,14 @@
 
 var Avatar = React.createClass({
   render: function() {
+    var classes = React.addons.classSet({
+      'avatar': true,
+      'avatar-small': this.props.size === 'small',
+      'avatar-default': !('size' in this.props)
+    });
+
     return (
-      <div className="avatar avatar-default">
+      <div className={classes}>
         <img src={this.props.person.gravatar_url} width="30" height="30" />
       </div>
     );
--- app/assets/javascripts/components/person.js.jsx
+++ app/assets/javascripts/components/person.js.jsx
@@ -4,7 +4,7 @@ var Person = React.createClass({
   render: function() {
     return (
       <div className="person">
-        <Avatar person={this.props.person} />
+        <Avatar person={this.props.person} size={'small'} />
         <strong>{this.props.person.email}</strong>
       </div>
     );
