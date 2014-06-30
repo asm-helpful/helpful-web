2f55198c2bba00e791477ff806123d406718fc4c
Chris Lloyd <christopher.lloyd@gmail.com>
2014-06-30 15:10:07 -0700

    Include person's name if present

--- app/assets/javascripts/components/person.js.jsx
+++ app/assets/javascripts/components/person.js.jsx
@@ -7,8 +7,25 @@ var Person = React.createClass({
         <span className="person-avatar">
           <Avatar person={this.props.person} size={'small'} />
         </span>
-        {this.props.person.email}
+
+        {this.label()}
       </div>
     );
+  },
+
+  label: function() {
+    if(this.props.person.name) {
+      return (
+        <span>
+          <strong>{this.props.person.name}</strong>
+          &nbsp;
+          <span className="text-muted">{this.props.person.email}</span>
+        </span>
+      )
+    } else {
+      return (
+        <strong>{this.props.person.email}</strong>
+      )
+    }
   }
 });
