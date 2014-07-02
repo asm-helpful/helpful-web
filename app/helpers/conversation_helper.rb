92f1a725dd53c4e1257d21d8b8c219081453d67a
Manish Valechha <manishval@hotmail.com>
2014-07-01 20:39:04 -0400

    Fix bootstrap error class

--- app/helpers/conversation_helper.rb
+++ app/helpers/conversation_helper.rb
@@ -3,8 +3,8 @@ module ConversationHelper
     case level.to_sym
       when :notice then "alert alert-info"
       when :success then "alert alert-success"
-      when :error then "alert alert-error"
-      when :alert then "alert alert-error"
+      when :error then "alert alert-danger"
+      when :alert then "alert alert-danger"
     end
   end
 
