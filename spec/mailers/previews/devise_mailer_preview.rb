30644837831d02716fb0055a263b62a77c320060
vanstee <patrick@vanstee.me>
2014-07-14 15:45:47 -0400

    Restyle password reset email with bootstrap

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -1,13 +1,19 @@
 class DeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.new({email: 'chuck@example.com'}), 'invitation-token')
+    Devise::Mailer.confirmation_instructions(user, 'invitation-token')
   end
 
   def reset_password_instructions
-    Devise::Mailer.reset_password_instructions(User.new({email: 'chuck@example.com'}), 'invitation-token')
+    Devise::Mailer.reset_password_instructions(user, 'invitation-token')
   end
 
   def unlock_instructions
-    Devise::Mailer.unlock_instructions(User.new({email: 'chuck@example.com'}), 'invitation-token')
+    Devise::Mailer.unlock_instructions(user, 'invitation-token')
+  end
+
+  def user
+    user = User.create(email: 'chuck@example.com')
+    user.create_person(name: 'Chuck Smith', email: 'chuck@example.com')
+    user
   end
 end
