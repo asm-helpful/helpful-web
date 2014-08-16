06912cecb10da47f89abdc8707314dfa93974a17
lachlanjc <lachlan.campbell@icloud.com>
2014-08-16 09:06:07 -0400

    #759 Use error panels for Devise errors

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,29 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="row">
+      <div class="panel panel-error">
+        <div class="panel-title">
+          <span class="geomicon geomicon-alert"></span>
+          #{sentence}
+        </div>
+        <ul>#{messages}</ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    resource.errors.empty? ? false : true
+  end
+
+end
